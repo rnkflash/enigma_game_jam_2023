7cf24a55853d4680dc9b935a5424b77ccddab5f3
StepanMaksimov <stepan.maximov@mytona.com>
2023-02-16 05:41:07 +0800

    inventory

--- Assets/Content/Scripts/character/Player.cs
+++ Assets/Content/Scripts/character/Player.cs
@@ -7,4 +7,12 @@ public class Player : Singleton<Player>
     public string toScene = null;
     public string toExit = null;
     public bool playerWasSprinting = false;
+
+    public Dictionary<ItemData, int> inventory = new Dictionary<ItemData, int>();
+
+    public void AddItem(ItemData item, int amount) {
+        if (!inventory.ContainsKey(item))
+            inventory[item] = 0;
+        inventory[item] += amount;
+    }
 }
--- Assets/Content/Scripts/items/Item.cs
+++ Assets/Content/Scripts/items/Item.cs
@@ -6,6 +6,7 @@ using DG.Tweening;
 public class Item : MonoBehaviour
 {
     public ItemData item;
+    public int amount = 1;
     private InteractiveObject interactiveObject;
 
     void Start() {
@@ -18,12 +19,14 @@ public class Item : MonoBehaviour
     }
 
     public void PickUp(PlayerInteraction interactor) {
-        Debug.Log($"Picked up {item.itemName}. {item.description}");
         interactiveObject.selectable = false;
         GetComponent<Rigidbody>().isKinematic = true;
         GetComponent<Rigidbody>().useGravity = false;
         GetComponent<Collider>().enabled = false;
         transform.DOScale(0f, 0.25f);
-        transform.DOMove(interactor.pickupPoint.position, 0.25f).OnComplete(()=>Destroy(gameObject));
+        transform.DOMove(interactor.pickupPoint.position, 0.25f).OnComplete(()=> {
+            Player.Instance.AddItem(item, amount);
+            Destroy(gameObject);
+        });
     }
 }
