7b6c340355bb3d1a04389b0de88d372964179f4c
StepanMaksimov <stepan.maximov@mytona.com>
2023-02-20 00:42:56 +0800

    save exosuit

--- Assets/Content/Scripts/items/PickedUpExosuit.cs
+++ Assets/Content/Scripts/items/PickedUpExosuit.cs
@@ -23,7 +23,7 @@ public class PickedUpExosuit : MonoBehaviour
 
         if (message.item.id == ItemId.Exosuit) {
             character.SetupCostume(true, character.pistoletoMode);
-
+            Player.Instance.cosmonaft = true;
         }
     }
 }
